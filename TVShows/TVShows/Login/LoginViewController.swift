fa6cd2e85762a70166dc3bc2fffd09b7c5644746
Borna Majstorovic <bmajstorovic8@gmail.com>
2019-07-14 12:11:11 +0200

    HW3 added loginViewControler and loginStoryBoard, added  podfile SVProgressHUD, todo: better auto layout

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -10,56 +10,65 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    
     // MARK: Outlets
-    @IBOutlet private weak var switchButton: UIButton!
-    @IBOutlet private weak var touchCountLabel: UILabel!
-    @IBOutlet private weak var onTouchIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
     
     // MARK: Properties
-    private(set) var touchCount = 0 {
-        didSet {
-            touchCountLabel.text = "Touch count: \(touchCount)"
-        }
-    }
+    private var checkBoxCount = 0
+   
+    
     // MARK: Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpView()
-        automaticStart()
+        loginButtonOutlet.layer.cornerRadius = 6
+        checkBoxButton.setImage(UIImage(named: "ic-checkbox-empty"), for: .normal)
+        checkBoxButton.setImage(UIImage(named: "ic-checkbox-filled"), for: .selected)
+        
     }
     
+    
     // MARK: Actions
-    @IBAction func touchSwitchButton(_ sender: UIButton) {
-        //  print("First HomeWork 🥳")
+
+    @IBAction func checkBoxButtonAction(_ sender: UIButton) {
         
-        // TODO: touchCount.isMultiple(of: 2) not working?
+    }
+    @IBAction func logInButton(_ sender: UIButton) {
         
-        touchCount += 1
-        if touchCount%2 == 0 {
-            sender.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
-            touchCountLabel.textColor = #colorLiteral(red: 1, green: 0.1909307073, blue: 0.190708672, alpha: 1)
-        } else {
-            sender.backgroundColor = #colorLiteral(red: 1, green: 0.1909307073, blue: 0.190708672, alpha: 1)
-            touchCountLabel.textColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
-        }
-        
-        if onTouchIndicator.isAnimating {
-            onTouchIndicator.stopAnimating()
-        } else {
-            onTouchIndicator.startAnimating()
-        }
+    }
+    
+    @IBAction func createAccountButton(_ sender: UIButton) {
     }
     
     // MARK: Class methods
-    private func setUpView(){
-        switchButton.layer.cornerRadius = 10
-    }
     
-    private func automaticStart(){
-        onTouchIndicator.startAnimating()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.onTouchIndicator.stopAnimating()
+//    func setUpLoginButton() {
+//        loginButtonOutlet.layer.cornerRadius = 6
+//
+//    }
+//
+//    func setUpCheckBoxButton() {
+//        checkBoxButton.setImage(UIImage(named: "ic-checkbox-empty"), for: .normal)
+//        checkBoxButton.setImage(UIImage(named: "ic-checkbox-filled"), for: .selected)
+//
+//
+//    }
+    
+    @IBAction func checkMarkTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            
+        }) { (success) in
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.isSelected = !sender.isSelected
+                sender.transform = .identity
+            }, completion: nil)
         }
     }
+    
+   
 }
